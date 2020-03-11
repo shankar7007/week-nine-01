3272cf3f5ff39c6350eebd6c32814aa0b825dc8f
shankar7007 <patelshankar29@yahoo.com>
2020-03-11 20:18:12 +0000

    ravihomework07

---
+++ src/Colours.java
@@ -0,0 +1,15 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Colours {
+        public static void main(String[] args) {
+            List<String> list_Strings = new ArrayList<String>();
+            list_Strings.add("Red");
+            list_Strings.add("Green");
+            list_Strings.add("Orange");
+            list_Strings.add("White");
+            list_Strings.add("Black");
+            System.out.println(list_Strings);
+        }
+    }
+
---
+++ src/ElementsArray.java
@@ -0,0 +1,23 @@
+import java.util.*;
+
+public class ElementsArray {
+
+    // Java  program to iterate over an ArrayList
+// using for loop
+    public static void main(String[] args)
+    {
+
+        // initializing ArrayList
+        List<Integer> numbers = Arrays.asList(1, 2, 3,
+                4, 5, 6, 7, 8);
+
+        // For Loop for iterating ArrayList
+        for (int i = 0; i < numbers.size(); i++)
+            System.out.print(numbers.get(i) + " ");
+    }
+}
+
+
+
+
+
